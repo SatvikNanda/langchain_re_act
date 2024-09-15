f998b8ea73752cd5760fab6a3762b446f8f98f6b
satvik nanda <satvik.nanda@gmail.com>
2024-09-15 23:17:18 +0530

    generated callbacks

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 #langchain hub is a very famous marketplace for prompts where people share their prompts that come in of great use 
 from typing import Union, List
-
 from dotenv import load_dotenv
+from langchain.agents import tool
+from langchain.agents.format_scratchpad import format_log_to_str
 from langchain.agents.output_parsers import ReActSingleInputOutputParser
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.schema import AgentAction, AgentFinish
-from langchain.tools import Tool, tool
+from langchain.tools import Tool
 from langchain.tools.render import render_text_description
+from callbacks import AgentCallBackHandler
+
 
 
 load_dotenv()
@@ -65,19 +68,20 @@ if __name__ == "__main__":
     Begin!
 
     Question: {input}
-    Thought:
+    Thought: {agent_scratchpad}
     """
 
     prompt = PromptTemplate.from_template(template=template).partial(
         tools=render_text_description(tools), tool_names=", ".join([t.name for t in tools])
     )
 
-    llm = ChatOpenAI(temperature=0, stop = ["\nObservation", "Observation"])
-
+    llm = ChatOpenAI(temperature=0, stop = ["\nObservation", "Observation"], callbacks=[AgentCallBackHandler()])
+    intermediate_steps = []
 
     agent = (
         {
             "input": lambda x: x["input"],
+            "agent_scratchpad": lambda x: format_log_to_str(x["agent_scratchpad"])
         }
         | prompt
         | llm
@@ -86,7 +90,8 @@ if __name__ == "__main__":
 
     agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
         {
-            "input": "What is the length of 'DOG' in characters?",
+            "input": "What is the length of 'HAPPY' in characters?",
+            "agent_scratchpad": intermediate_steps
         }
     )
 
@@ -99,3 +104,17 @@ if __name__ == "__main__":
 
         observation = tool_to_use.func(str(tool_input))
         print(f"{observation=}")
+        intermediate_steps.append((agent_step, str(observation)))
+
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+        {
+            "input": "What is the length of 'HAPPY' in characters?",
+            "agent_scratchpad": intermediate_steps
+        }
+    )
+
+    print(agent_step)
+
+    if isinstance(agent_step, AgentFinish):
+        print("### AgentFinish ###")
+        print(agent_step.return_values)
